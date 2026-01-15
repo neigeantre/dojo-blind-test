639e74dfe401b6ec3f75fe30495e53ca3378656a
neigeantre <covillesheer@gmail.com>
2026-01-15 23:16:21 +0100

    custom generation script

--- scripts/generate-spotify-client.js
+++ scripts/generate-spotify-client.js
@@ -27,22 +27,101 @@ function generateType(typeName, typeSchema) {
 }
 
 function getGeneratedCode(typeName, typeSchema) {
-  const generatedType = getGeneratedType(typeSchema);
+  const imports = new Set()
+  const generatedType = getGeneratedType(typeSchema, imports);
 
-  return `export type ${typeName} = ${generatedType};`;
+  let importCode = "";
+  imports.forEach((typeName) => {
+    importCode += `import { ${typeName} } from "./${typeName}";\n`;
+  });
+
+  if (importCode != "") {
+    importCode += "\n"
+  }
+
+  return `${importCode}export type ${typeName} = ${generatedType};`;
 }
 
-function getGeneratedType(typeSchema) {
+function getGeneratedType(typeSchema, imports) {
   const schemaType = typeSchema.type;
 
   // TO DO: Generate typescript code from schema
+  if (typeSchema.$ref != undefined) {
+    const path = typeSchema.$ref
+    const typeName = path.split("/").at(-1)
+    imports.add(typeName)
+    return typeName
+  }
+
+  if (typeSchema.oneOf != undefined) {
+    const types = typeSchema.oneOf.map((subTypeSchema) => {
+      return getGeneratedType(subTypeSchema, imports)
+    })
+    let generatedCode = "("
+    for (let index = 0; index < types.length - 1; index++) {
+      generatedCode += `${types[index]} | `;
+    }
+    generatedCode += types.at(-1)
+    generatedCode += ")"
+    
+    return generatedCode
+  }
+
+  if (typeSchema.allOf != undefined) {
+    const types = typeSchema.allOf.map((subTypeSchema) => {
+      return getGeneratedType(subTypeSchema, imports)
+    })
+    let generatedCode = ""
+    for (let index = 0; index < types.length - 1; index++) {
+      generatedCode += `${types[index]} & `;
+    }
+    generatedCode += types.at(-1)
+    
+    return generatedCode  
+  }
+
+  if (typeSchema.enum != undefined) {
+    const values = typeSchema.enum
+    let generatedCode = ""
+
+    for (let index = 0; index < values.length - 1; index++) {
+      generatedCode += `"${values[index]}" | `;
+    }
+    generatedCode += `"${values.at(-1)}"`
+
+    return generatedCode
+  }
+
   switch (schemaType) {
     case "number":
+      return "number"
     case "integer":
+      return "number"
     case "string":
+      return "string"
     case "boolean":
+      return "boolean"
     case "array":
+      const typeName = getGeneratedType(typeSchema.items, imports)
+      return `${typeName}[]`
     case "object":
+      if (typeSchema.properties != undefined) {
+        const requiredFields = typeSchema.required ?? []
+
+        let generatedCode = "{\n"
+        Object.keys(typeSchema.properties).forEach((propertyName) => {
+          const subTypeSchema = typeSchema.properties[propertyName]
+          const type = getGeneratedType(subTypeSchema, imports)
+
+          if (requiredFields.includes(propertyName)) {
+            generatedCode += `\t${propertyName}: ${type};\n`
+          } else {
+            generatedCode += `\t${propertyName}?: ${type};\n`
+          }
+        })
+        generatedCode += "}"
+        return generatedCode
+      }
     default:
       return "";
   }
